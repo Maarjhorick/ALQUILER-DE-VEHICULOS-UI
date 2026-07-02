665a7d8746061c0779ca5f2ff923a1af4ec91344
Edson Alabrin HUaman <jair20alabrin@gmail.com>
2026-07-02 03:59:03 -0500

    feat: agregar pagina 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,25 +3,54 @@ import { HomeComponent } from '../feature/home/home.component';
 import { NosotrosComponent } from '../feature/nosotros/nosotros.component';
 import { ContactanosComponent } from '../feature/contactanos/contactanos.component';
 import { NotFoundComponent } from '../shared/components/not-found/not-found.component';
+import { MainLayoutComponent } from '../layouts/main-layout/main-layout.component';
+import { ListarVehiculosComponent } from '../feature/vehiculos/listar-vehiculos/listar-vehiculos.component';
+import { ListarClientesComponent } from '../feature/clientes/listar-clientes/listar-clientes.component';
+import { ListarAlquileresComponent } from '../feature/alquileres/listar-alquileres/listar-alquileres.component';
 
 export const routes: Routes = [
-{
+  {
     path: '',
-    component: HomeComponent
-  },
+    component: MainLayoutComponent,
+    children: [
 
-  {
-    path: 'nosotros',
-    component: NosotrosComponent
-  },
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'vehiculos',
+        component: ListarVehiculosComponent
+      },
+      {
+        path: 'clientes',
+        component: ListarClientesComponent
+      },
+      {
+        path: 'alquileres',
+        component: ListarAlquileresComponent
+      },
 
-  {
-    path: 'contactanos',
-    component: ContactanosComponent
-  },
+      {
+        path: 'nosotros',
+        component: NosotrosComponent
+      },
 
-  {
-    path: '**',
-    component: NotFoundComponent
+      {
+        path: 'contacto',
+        component: ContactanosComponent
+      },
+      {
+        path: 'contactanos',
+        redirectTo: 'contacto',
+        pathMatch: 'full'
+      },
+
+      {
+        path: '**',
+        component: NotFoundComponent
+      }
+    ]
   }
+
 ];
