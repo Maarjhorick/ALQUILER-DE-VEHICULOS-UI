fa7b9ba3e65df9fb417aec7ff2e9e3266360549c
Edson Alabrin HUaman <jair20alabrin@gmail.com>
2026-07-03 00:04:17 -0500

    feat: agregar menú responsivo y página inicio

--- src/shared/components/header/header.component.ts
+++ src/shared/components/header/header.component.ts
@@ -3,15 +3,27 @@ import { RouterLink } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
